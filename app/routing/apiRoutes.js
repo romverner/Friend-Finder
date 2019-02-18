2ac75240d9da3ca7b462e173e155d6a3f7d11b40
Roman Verner <romverj@gmail.com>
2019-02-18 18:13:52 -0500

    Setting routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var surveyData = require("../data/friends");
+
+module.exports = function(app) {
+    
+    app.get("/api/friends", (req, res) => {
+        res.json(surveyData);
+    });
+
+    app.post("api/tables", (req, res) => {
+        surveyData.push(req.body);
+        res.json(true);
+    });
+};
