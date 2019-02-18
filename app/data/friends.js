2e2ac6f1c65c0c1774a6ff5b30cdc105454242ab
Roman Verner <romverj@gmail.com>
2019-02-18 18:18:27 -0500

    Sample answer

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,9 @@
 // Storing all survey results as objects in an array
+// Note: My scores are randomly generated, and do not indicate my actual answers ;)
 var surveyResults = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Roman",
+        "photo":"https://media.licdn.com/dms/image/C4E03AQGma2wXr5M67A/profile-displayphoto-shrink_200_200/0?e=1556150400&v=beta&t=iw_TOr4HZhNxogOXTBjnK-iKmrbSMgkBqdQMiNwXAxc",
         "scores":[
             5,
             1,
